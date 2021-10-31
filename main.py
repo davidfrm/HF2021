2e45440064ee10a8956507988e1c5b7e99f6183b
doooond <93309985+doooond@users.noreply.github.com>
2021-10-31 22:06:59 +0100

    Updated main.py
    This is a new function to plot

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+xpoints = np.array([1, 2, 6, 8, 14, 32])
+ypoints = np.array([3, 8, 1, 10, -5, 4])
 
 plt.plot(xpoints, ypoints)
 plt.show()
